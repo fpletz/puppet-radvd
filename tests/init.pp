0e560389092be3f9833dd4aaf7181e6ec24c8dbd
Franz Pletz <fpletz@fnordicwalking.de>
2013-01-05 08:15:11 +0100

    Update tests

--- tests/init.pp
+++ tests/init.pp
@@ -2,7 +2,7 @@ node default {
   include radvd
 
   radvd::interface { 'eth0':
-    options  => {
+    options => {
       'AdvSendAdvert'     => 'on',
       'MinRtrAdvInterval' => 10,
       'MaxRtrAdvInterval' => 30,
@@ -12,6 +12,19 @@ node default {
         'AdvOnLink'     => 'on',
         'AdvAutonomous' => 'on',
       },
+    },
+    rdnss => {
+      '2001:0DB8:2342:babe::1' => {
+        'AdvRDNSSLifetime' => 30,
+      },
+    },
+    dnssl => {
+      'branch.example.com example.com' => {
+        'AdvDNSSLLifetime' => 30,
+      },
+    },
+    routes => {
+      '2001:0DB8:2342:ccc::/64' => {},
     },
   }
 }

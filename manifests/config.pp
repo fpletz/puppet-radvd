1d4bc7ae0d14425bd9e380cec22dd31477b9f85c
Franz Pletz <fpletz@fnordicwalking.de>
2015-03-12 20:07:18 +0100

    Make puppet-lint happy

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,8 @@
 class radvd::config {
   concat {$radvd::conffile:
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
   }
   create_resources(radvd::interface, $radvd::interfaces)
 }
